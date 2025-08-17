bb615f657e1a88102cada453a37144c8c6673a33
Satya Raj Awasthi <77236280+SatyaRajAwasth1@users.noreply.github.com>
2025-08-17 16:25:51 +0545

    Remove vercel native build output generation to deploy through github pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,6 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
-import vercel from '@astrojs/vercel/static';
 
 export default defineConfig({
-  site: 'https://open-alumns-portal.vercel.app',
-  integrations: [tailwind()],
-  output: 'static',
-  adapter: vercel(),
-  vite: {
-    ssr: {
-      external: ['svgo'],
-    },
-  },
+  integrations: [tailwind()]
 });
